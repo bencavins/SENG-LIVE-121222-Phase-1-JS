0b6a6d9e133b22507df94ee2b4f3f99c6b7c202d
Ben Cavins <ben.cavins@flatironschool.com>
2023-01-18 11:00:47 -0500

    add api

--- 07_Communicating_with_External_APIS/src/index.js
+++ 07_Communicating_with_External_APIS/src/index.js
@@ -2,10 +2,16 @@ const resultsDiv = document.querySelector('#results');
 document.addEventListener('DOMContentLoaded', () => {
   const apiSearchForm = document.querySelector('#api-Search');
   
-  apiSearchForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const query = encodeURI(e.target.search.value);
-    console.log(query);
+  apiSearchForm.addEventListener('submit', (event) => {
 
+    event.preventDefault();
+    console.log(event.target.search)
+    const query = encodeURI(event.target.search.value);
+
+    fetch(`https://api.tvmaze.com/singlesearch/shows?q=${query}`)
+      .then(resp => resp.json())
+      .then(data => console.log(data))
+    
+    apiSearchForm.reset()
   })
 })
