117de8937cd0abfbd211d6b4b27517bef4eb5e63
Ben Cavins <ben.cavins@flatironschool.com>
2023-09-01 11:00:16 -0400

    poke

--- 07_Communicating_with_External_APIS/src/index.js
+++ 07_Communicating_with_External_APIS/src/index.js
@@ -1,11 +1,21 @@
-const resultsDiv = document.querySelector('#results');
-document.addEventListener('DOMContentLoaded', () => {
-  const apiSearchForm = document.querySelector('#api-Search');
-  
-  apiSearchForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const query = encodeURI(e.target.search.value);
-    console.log(query);
+const form = document.querySelector('#search')
+console.log(apiKey)
 
-  })
+form.addEventListener('submit', event => {
+    event.preventDefault()
+    const pokeName = event.target.name.value
+
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokeName}`)
+    .then(resp => resp.json())
+    .then(data => {
+      const results = document.querySelector('#results')
+      const img = document.createElement('img')
+      img.src = data.sprites.front_default
+      results.append(img)
+    })
+    .catch(resp => {
+      console.log('error')
+    })
 })
+
+results.innerHTML = '<h1>HI</h1>'
