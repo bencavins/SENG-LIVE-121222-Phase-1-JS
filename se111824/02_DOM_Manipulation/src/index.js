8155e409202304d330ba78bbba4b29476cd56ada
Ben Cavins <bencavins@gmail.com>
2024-11-18 13:46:25 -0500

    add 2

--- se111824/02_DOM_Manipulation/src/index.js
+++ se111824/02_DOM_Manipulation/src/index.js
@@ -1,9 +1,66 @@
 //BookStore has been moved to data.js 
 console.log(bookStore);
 
+const bookListElem = document.querySelector('#book-list')
+
 function formatPrice(price) {
   return '$' + Number.parseFloat(price).toFixed(2);
 }
+
+
+function renderHeader(bookStore) {
+  const nameElement = document.querySelector('#bookstore-name')
+  nameElement.textContent = bookStore.name
+}
+
+function renderFooter(bookStore) {
+  const storeElement = document.querySelector('#store')
+  const numberElement = document.querySelector('#number')
+  const addressElement = document.querySelector('#address')
+  
+  storeElement.textContent = bookStore.name
+  numberElement.textContent = bookStore.number
+  addressElement.textContent = bookStore.address
+}
+
+function renderBook(book) {
+  console.log(book)
+  const newBookElem = document.createElement('li')
+  newBookElem.setAttribute('class', 'list-li')
+  bookListElem.appendChild(newBookElem)
+
+  const title = document.createElement('h3')
+  title.textContent = book.title
+  newBookElem.appendChild(title)
+
+  const author = document.createElement('p')
+  author.textContent = book.author
+  newBookElem.appendChild(author)
+
+  const price = document.createElement('p')
+  price.textContent = formatPrice(book.price)
+  newBookElem.appendChild(price)
+
+  const img = document.createElement('img')
+  img.setAttribute('src', book.imageUrl)
+  newBookElem.appendChild(img)
+
+  const button = document.createElement('button')
+  button.textContent = 'Delete'
+  newBookElem.appendChild(button)
+}
+
+renderHeader(bookStore)
+renderFooter(bookStore)
+
+bookStore.inventory.forEach((book) => renderBook(book))
+
+
+// renderBook({
+//   title: 'test',
+//   author: 'bob',
+//   price: 4.55
+// })
 
 // create a function called renderBook(book)
 // it will take a book object as an argument
