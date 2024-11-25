cb315e3f221bdb1b35b00a5c705ab752d79e62fc
Ben Cavins <bencavins@gmail.com>
2024-11-25 12:57:51 -0500

    do lec 7

--- se111824/07_Communicating_with_External_APIS/src/index.js
+++ se111824/07_Communicating_with_External_APIS/src/index.js
@@ -1,8 +1,27 @@
 const resultsDiv = document.querySelector('#results');
 const apiSearchForm = document.querySelector('#api-Search');
+const tvList = document.querySelector('#tv-list')
+
+function renderShow(show) {
+  // console.log(show)
+  const li = document.createElement('li')
+  const h3 = document.createElement('h3')
+  const img = document.createElement('img')
+  img.src = show.show.image.medium
+  h3.textContent = show.show.name
+  li.appendChild(h3)
+  li.appendChild(img)
+  tvList.append(li)
+}
 
 apiSearchForm.addEventListener('submit', (e) => {
   e.preventDefault();
   const query = encodeURI(e.target.search.value);
   console.log(query);
+
+  tvList.innerHTML = ""
+
+  fetch(`https://api.tvmaze.com/search/shows?q=${query}`)
+  .then(resp => resp.json())
+  .then(data => data.forEach(renderShow))
 })
